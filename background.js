c4c9ff2e5b03383402df526c9dce4f22dcf5b76a
Shih-Hao Chen <samuelshchen@gmail.com>
2021-11-30 15:16:34 +1300

    correctly parses test cases

--- background.js
+++ background.js
@@ -1,33 +1,5 @@
-let color = "#3aa757";
+// This service worker runs in the background
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Hello World!");
-
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
-
-  console.log("Installed - clipboard:");
-  console.log(navigator.clipboard);
-});
-
-const formatCode = async () => {
-  console.log("Hello World Again");
-  console.log("Hello!");
-  try {
-    console.log(navigator.clipboard);
-    const text = await navigator.clipboard.readText();
-    console.log("Read text!", text);
-    await navigator.clipboard.writeText(text + "heheheh");
-
-    console.log("Finished");
-  } catch (e) {
-    console.log("Some error happened:", e);
-  }
-};
-// background.js
-chrome.commands.onCommand.addListener((command) => {
-  console.log(`Command "${command}" triggered`);
-  if (command === "format") {
-    formatCode();
-  }
+  console.log("Service worker is running/listening");
 });
